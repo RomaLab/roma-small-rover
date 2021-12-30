9c36f22f5d9bf1e23cf731435134507d2e993bbb
lennylee <1021620522@qq.com>
2021-12-30 22:45:19 +0800

    update .gitignore

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/roma/roma-small-rover/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/roma/roma-small-rover/devel;/opt/ros/noetic'.split(';') if '/home/roma/roma-small-rover/devel;/opt/ros/noetic' != '' else []
